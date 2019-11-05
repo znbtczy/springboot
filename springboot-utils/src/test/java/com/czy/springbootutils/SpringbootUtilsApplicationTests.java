4427086020038375fcb3a848c1e1f6b367f36ca4
曹忠月 <caozhongyue@filmtoc.com>
2019-11-05 11:00:06 +0800

    Initial commit

---
+++ springboot-utils/src/test/java/com/czy/springbootutils/SpringbootUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czy.springbootutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
